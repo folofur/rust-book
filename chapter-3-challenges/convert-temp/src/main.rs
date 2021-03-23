8ffa8d753c257dafc55c04e111111197bce3e1f0
Sean Ruffolo <portlandlink@gmail.com>
2021-03-23 11:38:30 -0400

    match or...?

--- chapter-3-challenges/convert-temp/src/main.rs
+++ chapter-3-challenges/convert-temp/src/main.rs
@@ -1,7 +1,15 @@
+use std::io;
+
 fn main() {
     println!("Which temp conversion would you like?");
     println!("For celcius to farhenheit, press 1");
     println!("For farhenheit to celcius, press 2");
 
+    let mut decide = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to read line");
+
+    let decide: String = 
 
 }
