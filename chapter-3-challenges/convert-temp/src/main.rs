fcb031b3dd9c9c3e0865d7591be2400b654253dc
Sean Ruffolo <portlandlink@gmail.com>
2021-03-23 11:33:56 -0400

    prompt

--- chapter-3-challenges/convert-temp/src/main.rs
+++ chapter-3-challenges/convert-temp/src/main.rs
@@ -1,5 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Which temp conversion would you like?");
+    println!("For celcius to farhenheit, press 1");
+    println!("For farhenheit to celcius, press 2");
 
-    
+
 }
