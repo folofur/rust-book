8e93cf082addb4fd32eca0428ed0e8c97b8cbfc4
Sean Ruffolo <portlandlink@gmail.com>
2021-04-07 10:34:52 -0400

    it works, really fun

--- rectangular-struct/src/main.rs
+++ rectangular-struct/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     struct Rectangle {
         width: u32,
-        height: u32
+        height: u32,
     }
 
     impl Rectangle {
@@ -14,16 +14,27 @@ fn main() {
         }
 
         fn can_fit_rotated(&self, other: &Rectangle) -> bool {
-
-            if (self.width > other.width && self.height > other.height) {
+            if self.width > other.width && self.height > other.height {
                 true
-            } else if (self.width > other.height && self.height > other.width) {
+            } else if self.width > other.height && self.height > other.width {
                 true
-            } else false
-           
+            } else {
+                false
+            }
         }
     }
-    let rect1 = Rectangle { width: 30, height: 50};
-    let rect2 = Rectangle { width: 30, height: 50};
-    let rect3 = Rectangle { width: 30, height: 50};
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 40,
+        height: 20,
+    };
+
+    // let rect3 = Rectangle { width: 10, height: 5};
+
+    println!("{}", rect1.find_area());
+    println!("{}", rect1.can_fit(&rect2));
+    println!("{}", rect1.can_fit_rotated(&rect2))
 }
