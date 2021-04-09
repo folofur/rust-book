bd5bf9e9b6fa57437ce439b70624c1e84e72de25
Sean Ruffolo <portlandlink@gmail.com>
2021-04-09 10:24:45 -0400

    colors enum

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+enum Color {
+    Red,
+    Green,
+    Blue,
+    RgbColor(u8, u8, u8)
 }
+fn enums() 
+{
+    let c:Color = Color::Red;
+
+    match c {
+        Color::Red => println!("R"),
+        Color::Green => println!("G"),
+        Color::Blue => println!("B"),
+        Color::RgbColor(0,0,0) => println!("no color"),
+        Color::RgbColor(r,g,b) => println!("RGB: {}, {}, {}" , r, g, b)
+    }
+}
+fn main() {}
