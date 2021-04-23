2d94cdf2f6e517d83080658a0cd27034f2140826
Sean Ruffolo <portlandlink@gmail.com>
2021-04-23 10:33:54 -0400

    collections

---
+++ collections/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let v: Vec<i32> = Vec::new();
+
+    let initial_v = vec![2,3,4];
+
+    let mut update_v = Vec::new();
+
+    update_v.push(5);
+    update_v.push(6);
+    update_v.push(5);
+
+    
+}
--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -9,15 +9,13 @@ mod tests {
 mod front_of_house {
     mod hosting {
         fn add_to_waitlist() {
-            return
+            return;
         }
-        fn seat_at_table() {
-
-        }
+        fn seat_at_table() {}
     }
-    mod serving{
-        fn take_order(){}
-        fn serve_order(){}
-        fn take_payment(){}
+    mod serving {
+        fn take_order() {}
+        fn serve_order() {}
+        fn take_payment() {}
     }
 }
