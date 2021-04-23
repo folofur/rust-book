4c716f8da4ab89d346bb51a2d1fce10b1648e08e
Sean Ruffolo <portlandlink@gmail.com>
2021-04-23 11:12:56 -0400

    vec

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -9,5 +9,15 @@ fn main() {
     update_v.push(6);
     update_v.push(5);
 
-    
+
+    // READ vectors
+    let third: i32 = &initial_v[2];
+
+    // or...
+    match v.get(2) {
+        Some(third) => println!("The 3rd element is {}", third),
+        None => println!("no 3rd element present"),
+    }
+
+
 }
