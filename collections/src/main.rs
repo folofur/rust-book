e3ab162c190906eb3d01f48761daac7de0f16d1e
Sean Ruffolo <portlandlink@gmail.com>
2021-04-24 10:31:11 -0400

    ref

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,23 +1,23 @@
 fn main() {
-    let v: Vec<i32> = Vec::new();
+    // let v: Vec<i32> = Vec::new();
 
-    let initial_v = vec![2,3,4];
+    let initial_v = vec![2, 3, 4];
 
-    let mut update_v = Vec::new();
+    let mut update_v: Vec<i32> = Vec::new();
 
-    update_v.push(5);
-    update_v.push(6);
-    update_v.push(5);
-
+    update_v.push(5i32);
+    update_v.push(6i32);
+    update_v.push(5i32);
 
     // READ vectors
-    let third: i32 = &initial_v[2];
+    let third: &i32 = &initial_v[2];
+    println!("the 3rd element is {}", third);
 
     // or...
-    match v.get(2) {
+    // this way is advantageous because it will not cause the program to crash
+    // because of a reference to some non-existent item. 
+    match initial_v.get(2) {
         Some(third) => println!("The 3rd element is {}", third),
         None => println!("no 3rd element present"),
     }
-
-
 }
