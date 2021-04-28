b729b520d65fcf85b170ed86f44d4789c22a7be8
Sean Ruffolo <portlandlink@gmail.com>
2021-04-28 10:59:41 -0400

    hashmaps

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+
+
 fn main() {
     // let v: Vec<i32> = Vec::new();
 
@@ -11,15 +14,15 @@ fn main() {
 
     // READ vectors
     let third: &i32 = &initial_v[2];
-    println!("the 3rd element is {}", third);
+    // println!("the 3rd element is {}", third);
 
     // or...
     // this way is advantageous because it will not cause the program to crash
     // because of a reference to some non-existent item. 
-    match initial_v.get(2) {
-        Some(third) => println!("The 3rd element is {}", third),
-        None => println!("no 3rd element present"),
-    }
+    // match initial_v.get(2) {
+    //     Some(third) => println!("The 3rd element is {}", third),
+    //     None => println!("no 3rd element present"),
+    // }
 
     //new String can initialize just like new vec.
     // a String is actually a wrapper around a vector. 
@@ -33,6 +36,18 @@ fn main() {
     let alter_s = "characters to Stringify".to_string();
     alt_s.push_str("asdf");
 
-    
 
+
+    let mut scores = HashMap::new();
+    scores.insert(String::from("red"), 10);
+
+    // create two vectors
+    let teams = vec![String::from("Blue"), String::from("red")];
+    let scores = vec![10, 13];
+    // create a vector of tuples from those two vectors with zip
+    let game: HashMap<_, _> = teams.iter().zip(scores.iter()).collect();
+    // hashmaps assume ownership of any data without the "Copy" trait.
+    // so "game" has destroyed the strings in the "teams" vector
+
+    
 }
