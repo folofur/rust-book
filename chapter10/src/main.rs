4cfd18b5189ae40ca5b930ecead9515576b833fe
Sean Ruffolo <portlandlink@gmail.com>
2021-05-19 13:16:55 -0400

    m

--- chapter10/src/main.rs
+++ chapter10/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let firstResult = largest(&number_list);
+    let secondResult = largest(&char_list);
+
+    println!("The first result is {}", firstResult);
+    println!("The 2nd result is {}", secondResult);
 }
+
+
