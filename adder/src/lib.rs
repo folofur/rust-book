04c8cfda9ae75d4003eef5074bed0a151c4f466e
Sean Ruffolo <portlandlink@gmail.com>
2021-05-25 08:13:08 -0400

    rect

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -9,3 +9,15 @@ mod tests {
         panic!("never meant to work");
     }
 }
+
+#[derive(Debug)]
+pub struct Rectangle {
+    length: u32,
+    width: u32,
+}
+
+impl Rectangle {
+    pub fn can_hold(&self, other: Rectangle) -> bool {
+        self.length > other.length && self.width > other.width
+    }
+}
