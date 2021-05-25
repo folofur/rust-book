4ced454c0d4e38fd3eb17af547e8a3bb8d671ef1
Sean Ruffolo <portlandlink@gmail.com>
2021-05-25 08:36:20 -0400

    can rectangle fit test with assert macro

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,5 +1,6 @@
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
@@ -7,6 +8,19 @@ mod tests {
     #[test]
     fn i_fail() {
         panic!("never meant to work");
+    }
+    #[test]
+    fn assert_hold() {
+        let larger = Rectangle {
+            length: 12,
+            width: 9,
+        };
+        let smaller = Rectangle {
+            length: 3,
+            width: 4,
+        };
+
+        assert!(larger.can_hold(smaller));
     }
 }
 
