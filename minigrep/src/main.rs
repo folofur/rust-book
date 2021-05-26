b288fa624044a9a2b20821a5dc03aaa98f8311a5
Sean Ruffolo <portlandlink@gmail.com>
2021-05-26 15:54:46 -0400

    text file and fs to read content

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,12 +1,16 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
 
     let query = &args[1];
     let filename = &args[2];
 
     println!("You're searching for: {}", query);
     println!("in this file: {}", filename);
+
+    let contents = fs::read_to_string(filename).expect("Unable to read that specified file.");
+
+    println!("Which does contain this text: \n{}", contents);
 }
