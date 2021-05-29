ce831eccb2d503fb98f9ff8d4a3f10944ff2e4ee
Sean Ruffolo <portlandlink@gmail.com>
2021-05-29 12:31:07 -0400

    function out parse arguments

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,9 +3,7 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
-    let query = &args[1];
-    let filename = &args[2];
+    let (query, filename) = parse_config(&args);
 
     println!("You're searching for: {}", query);
     println!("in this file: {}", filename);
@@ -13,4 +11,11 @@ fn main() {
     let contents = fs::read_to_string(filename).expect("Unable to read that specified file.");
 
     println!("Which does contain this text: \n{}", contents);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
 }
